c6407e8c82a09ca85db147a35c90c139564a5a21
lennart falter <lennart.falter@gmail.com>
2018-11-27 21:17:45 +0100

    added manual playing option

--- src/main/java/ki18/jfour/Arena.java
+++ src/main/java/ki18/jfour/Arena.java
@@ -1,8 +1,12 @@
 package ki18.jfour;
 
+import java.util.Scanner;
+
 public class Arena {
 
-	private static int CALCULATION_TIME = 60000 * 5;
+//	private static int CALCULATION_TIME = 60000 * 5;
+	private static int CALCULATION_TIME = 1000;
+	private static boolean MANUAL = true;
 
 	/*
 	 * welcome to the summoners rift
@@ -15,30 +19,47 @@ public class Arena {
 		Board b = new Board(7, 10);
 
 		int i = -1;
-		int maxMoves = 1;
-		while (b.getWinner() == Player.NONE) {
-			i++;
-			if (i >= maxMoves) {
-				System.out.println("Max moves reached.");
-				return;
+		int maxMoves = 100;
+		Move move = null;
+		Scanner scanner = null;
+		try {
+			if (MANUAL) {
+				scanner = new Scanner(System.in);
+			}
+			while (b.getWinner() == Player.NONE) {
+				i++;
+				if (i >= maxMoves) {
+					System.out.println("Max moves reached.");
+					return;
+				}
+
+				if (i % 2 == 0) {
+					move = getMove(CALCULATION_TIME, b, NEWCONTENDERXXXXXX);
+				} else {
+					if (MANUAL) {
+						System.out.println("ENTER YOUR MOVE: ");
+						int column = scanner.nextInt() - 1;
+						move = new Move(column);
+					} else {
+						move = getMove(CALCULATION_TIME, b, myAi);
+					}
+				}
+				System.out.println("Executing move: " + move);
+				b = b.executeMove(move);
+				System.out.println(b);
 			}
 
-			if (i % 2 == 0) {
-				b = makeMove(CALCULATION_TIME, b, NEWCONTENDERXXXXXX);
-			} else {
-				b = makeMove(CALCULATION_TIME, b, myAi);
+			System.out.println(b.getWinner());
+			System.out.println(i);
+		} catch (Exception e) {
+			if (scanner != null) {
+				scanner.close();
 			}
-			System.out.println(b);
-//			System.out.println();
-//			System.out.println();
 		}
-
-		System.out.println(b.getWinner());
-		System.out.println(i);
 	}
 
 	@SuppressWarnings("deprecation")
-	private static Board makeMove(final int time, final Board b, AI ai) throws InterruptedException {
+	private static Move getMove(final int time, final Board b, AI ai) throws InterruptedException {
 		final Thread t = new Thread(() -> ai.start(b));
 		t.setUncaughtExceptionHandler((tt, e) -> {
 			if (e instanceof ThreadDeath) {
@@ -50,7 +71,7 @@ public class Arena {
 		t.start();
 		Thread.sleep(time);
 		t.stop();
-		return b.executeMove(ai.getBestMove().get());
+		return ai.getBestMove().get();
 	}
 
 }
